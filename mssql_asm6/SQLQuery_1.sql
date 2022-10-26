baaa010830d75cba30e12e826c93ba5926e7a9b7
huevu102 <98642149+huevu102@users.noreply.github.com>
2022-10-26 18:51:55 +0700

    Update SQLQuery_1.sql

--- mssql_asm6/SQLQuery_1.sql
+++ mssql_asm6/SQLQuery_1.sql
@@ -1,75 +1,74 @@
-create table Genres(
-    Name Nvarchar(255) not null,
-    Id int primary key identity(1,1)
+create table As6_Genres(
+   Name Nvarchar(255) not null,
+   Id int primary key identity(1,1)
 );
 
-Create table Publishers(
-    Address nvarchar(255) not null,
-    Name nvarchar(255) not null,
-    Id int primary key identity(1,1)
+Create table As6_Publishers(
+   Address nvarchar(255) not null,
+   Name nvarchar(255) not null,
+   Id int primary key identity(1,1)
 );
 
-create table Authors(
-    Name nvarchar(255) not null,
-    Id int primary key identity(1,1)
+create table As6_Authors(
+   Name nvarchar(255) not null,
+   Id int primary key identity(1,1)
 );
 
-create table Books(
-    NumberOfPublication int not null,
-    Code varchar(10) not null unique,
-    Name nvarchar(255) not null unique,
-    YearOfPublication varchar(10) not null,
-    Summary ntext not null,
-    Price decimal(12,4) not null check(Price > 0),
-    Quantity int not null check(Quantity >= 0),
-    GenreId int not null foreign key references Genres(Id),
-    PublisherId int not null foreign key references Publishers(Id),
+create table As6_Books(
+   NumberOfPublication int not null,
+   Code varchar(10) primary key,
+   Name nvarchar(255) not null unique,
+   YearOfPublication varchar(10) not null,
+   Summary ntext not null,
+   Price decimal(12,4) not null check(Price > 0),
+   Quantity int not null check(Quantity >= 0),
+   GenreId int not null foreign key references As6_Genres(Id),
+   PublisherId int not null foreign key references As6_Publishers(Id),
 );
 
-create table BookAuthor(
-    BookCode varchar(10) not null foreign key references Books(Code),
-    AuthorId int not null foreign key references Authors(Id)
+create table As6_BookAuthor(
+   BookCode varchar(10) not null foreign key references As6_Books(Code),
+   AuthorId int not null foreign key references As6_Authors(Id)
 );
 
-
-insert into Genres (Name)
+insert into As6_Genres (Name)
 values(N'Khoa học xã hội');
 
-Select * from Genres;
+Select * from As6_Genres;
 
-insert into Publishers (Address,Name)
+insert into As6_Publishers (Address,Name)
 values(N'53 Nguyễn Du, Hai Bà Trưng, Hà Nội',N'Tri Thức');
 
-Select * from Publishers;
+Select * from As6_Publishers;
 
-insert into Authors (Name)
+insert into As6_Authors (Name)
 values(N'Eran Katz');
 
-Select * from Authors;
+Select * from As6_Authors;
 
-insert into Books (NumberOfPublication,Code,Name,YearOfPublication,Summary,Price,Quantity,GenreId,PublisherId)
+insert into As6_Books (NumberOfPublication,Code,Name,YearOfPublication,Summary,Price,Quantity,GenreId,PublisherId)
 values(1,'B001',N'Trí tuệ Do Thái','2010',
 N'Bạn có muốn biết: Người Do Thái sáng tạo ra cái gì và nguồn gốc trí tuệ của họ xuất phát từ đâu không? Cuốn sách này sẽ dần hé lộ những bí ẩn về sự thông thái của người Do Thái, của một dân tộc thông tuệ với những phương pháp và kỹ thuật phát triển tầng lớp trí thức đã được giữ kín hàng nghìn năm như một bí ẩn mật mang tính văn hóa.'
 ,79000,100,1,1);
 
-Select * from Books;
+Select * from As6_Books;
 
-insert into BookAuthor(BookCode,AuthorId)
+insert into As6_BookAuthor(BookCode,AuthorId)
 values('B001',1);
 
-select * from BookAuthor;
+select * from As6_BookAuthor;
 
 --insert them 8 quyen sach
-insert into Authors(Name)
+insert into As6_Authors(Name)
 values(N'Blair T. Spalding'),(N'Stephen R. Covey'),(N'Dale Carnegie'),(N'Margaret Mitchell'),(N'Trần Văn Thắng'),(N'Dan Brown');
 
-insert into Genres(Name)
+insert into As6_Genres(Name)
 values(N'Phiêu lưu, bí ẩn'),(N'Tiểu thuyết');
 
-insert into Publishers(Address,Name)
+insert into As6_Publishers(Address,Name)
 values(N'18 Nguyễn Trường Tộ - Ba Đình - Hà Nội',N'Nhà xuất bản văn học');
 
-insert into Books (NumberOfPublication,Code,Name,YearOfPublication,Summary,Price,Quantity,GenreId,PublisherId)
+insert into As6_Books (NumberOfPublication,Code,Name,YearOfPublication,Summary,Price,Quantity,GenreId,PublisherId)
 values(1,'B002',N'Hành trình về Phương Đông','2009',
 N'kể về những trải nghiệm của một đoàn khoa học gồm các chuyên gia hàng đầu của Hội Khoa Học Hoàng Gia Anh được cử sang Ấn Độ nghiên cứu'
 ,85000,20,3,1),
@@ -95,36 +94,86 @@ N'Tiểu thuyết Thiên thần và ác quỷ sự kết hợp giữa truyện t
 N'Cuốn sách Kinh điển về Tư duy, Kỹ năng mà bất cứ ai cũng nên đọc.'
 ,120000,200,3,1);
 
-insert into BookAuthor(BookCode,AuthorId)
+insert into As6_BookAuthor(BookCode,AuthorId)
 values('B002',2),('B003',3),('B004',4),('B005',5),('B006',6),('B007',7),('B008',7),('B009',1);
 
---4 liet ke 5 cuon sach co gia ban cao nhat
-select top 50 percent * from Books order by Price desc;
---5 Tim nhung cuon sach co tieu de chua tu 'tin học'
-select Name from Books where Name like N'%tin học%';
---6 Liet ke nhung cuon sach bat dau bang chu T theo thu tu giam dan ve gia
-select Name from Books where Name like N'T%'order by Price desc;
---7 Liet ke sach cua nha suat ban tri thuc
-select Name from Books where PublisherId in
-    (select Id from Publishers where Name like N'Tri Thức');
---8 Lay thong tin chi tiet ve nha xuat ban cuon sach 'Tri tue Do Thai'
-select * from Publishers where Id in
-    (select PublisherId from Books where Name like N'Trí tuệ Do Thái');
---9 Hien thi thong tin sau ve nhung cuon sach: Ma sach, Ten sach, Nam xuat ban, Nha xuat ban, Loai sach
+--3. 
+select * from As6_Books where YearOfPublication >= 2008;
+--4. 
+select top 10 * from As6_Books order by Price desc;
+--5. 
+select * from As6_Books where Name like N'%Tin học%';
+--6. 
+select * from As6_Books where Name like 'T%' order by Price desc;
+--7.
+select * from As6_Books where PublisherId in 
+(select Id from As6_Publishers where Name like N'%Tri Thức%');
+--8. 
+select * from As6_Publishers where Id in 
+(select PublisherId from As6_Books where Name like N'Trí tuệ Do Thái');
+--9.
+select a.Code,a.Name as bookName,a.YearOfPublication, c.Name as pubName
+,b.Name as catName from As6_Books a
+left join As6_Genres b on a.GenreId = b.Id
+left join As6_Publishers c on a.PublisherId = c.Id;
 
---10 Tim cuon sach co gia ban cao nhat
-select top 1 * from Books order by Price desc;
---11 tim sach co so luong nhieu nhat trong kho
-select top 1 * from Books order by Quantity desc;
---12 tim sach cua tac gia 'Eran Katz'
-select Name from Books where Code in 
-    (select BookCode from BookAuthor where AuthorId in 
-        (select Id from Authors where Name like N'Eran Katz'));
---13 giam gia ban 10% nhung cuon sach xuat ban truoc nam 2008
+--10.
+select top 1 * from As6_Books order by Price desc;
+--11.
+ select top 1 * from As6_Books order by Quantity desc;
+ --12.
+ select * from As6_Books where code in 
+ (select BookCode from As6_BookAuthor where AuthorId in 
+ (select Id from As6_Authors where Name like 'Eran Katz'));
+ --13. 
+ update As6_Books set Price = Price*90/100 where YearOfPublication < 2008;
+ --14. 
+ select PublisherId,count(*) as slsach from As6_Books group by PublisherId;
+ --
+ select * from As6_Publishers a left join 
+ (select PublisherId,count(*) as slsach from As6_Books 
+ group by PublisherId) b on a.Id = b.PublisherId;
 
---14 Thong ke so dau sach cua moi nha xuat ban
-select count(Name) as SoLuongDauSach from Books group by PublisherId;
---15 Thong ke so dau sach cua moi loai sach
-select count(Name) as SoLuongDauSach from Books group by GenreId;
---16 Dat chi muc Index cho truong ten sach
-create index chi_muc_vat_ly on Books(Name);
+ --15.
+ select GenreId,count(*) as slsach from As6_Books group by GenreId;
+ --16.
+ create index bookname_index on As6_Books(Name);
+ --17.
+ create view thong_tin_sach as
+ select a.Code,a.Name, a.Price,b.Name as PubName, d.Name as authorName
+  from As6_Books a
+ left join As6_Publishers b on a.PublisherId = b.Id
+ left join As6_BookAuthor c on c.BookCode = a.Code
+ left join As6_Authors d on c.AuthorId = d.Id;
+
+--18.
+--
+create procedure them_sach @code varchar(10), @name nvarchar(255),
+@price decimal(12,4), @year varchar(10),@num int,@sumary ntext,@qty int,
+@genid int,@pubid int as
+insert into As6_Books(NumberOfPublication,Code,Name,YearOfPublication,Summary,
+Price,Quantity,GenreId,PublisherId)
+values(@num,@code,@name,@year,@sumary,@price,@qty,@genid,@pubid);
+--
+create procedure tim_sach @q nvarchar(100) as
+select * from As6_Books where Name like N'%'+@q+'%';
+--
+create procedure liet_ke_theo_chuyen_muc @genId int as
+select * from As6_Books where GenreId = @genId;
+
+--19.
+create trigger ko_xoa_sach 
+on As6_Books 
+after delete
+as
+if exists (select * from deleted where Quantity > 0)
+rollback transaction;
+
+--20.
+create trigger ko_xoa_danh_muc
+on As6_Genres
+after delete
+as 
+if exists (select * from deleted where Id in 
+(select * from As6_Books))
+ rollback transaction;
